e9e51c9111fdee6fdc2728c0000b66597c6631db
Ariel Feingold <feingold.ariel@gmail.com>
2018-02-13 20:45:50 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
-# Add  code here!
+require 'pry'
+
+def prime?(number)
+  if number < 2
+    false
+  else
+    f = 2
+    while f < number do
+      array = []
+      if number % f == 0
+        array << f
+        f += 1
+      else f += 1
+      end
+    end
+    array
+    binding.pry
+  end
+end
